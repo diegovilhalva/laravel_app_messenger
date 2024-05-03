0b8ecc759e03f9b876df7da69ac7698e886c775d
Diego Vilhlava <diegorocha450@yahoo.com>
2024-05-03 14:16:49 -0300

    create seeder database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Conversation;
 use App\Models\Group;
 use App\Models\Message;
 use App\Models\User;
+use Carbon\Carbon;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -37,6 +39,18 @@ class DatabaseSeeder extends Seeder
         Message::factory(1000)->create();
         $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
 
-        $conversations = $messages->groupBy();
+        $conversations = $messages->groupBy(function ($message) {
+            return collect([$message->sender_id,$message->receiver->receiver_id])
+            ->sort()->implode('_');
+        })->map(function ($groupedMessages){
+            return [
+                'user_id1' => $groupedMessages->first()->sender_id,
+                'user_id2' => $groupedMessages->first()->receiver_id,
+                'last_message_id' => $groupedMessages->last()->id,
+                'created_at'=> new Carbon(),
+                'updated_at' => new Carbon()
+            ];
+        })->values();
+        Conversation::insertOrIgnore($conversations->toArray());
     }
 }
