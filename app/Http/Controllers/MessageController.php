7fbfdaa23910e60b163404c72162d217ecf30c3e
Diego Vilhlava <diegorocha450@yahoo.com>
2024-05-08 13:35:48 -0300

    message model and cotroller created

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,101 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreMessageRequest;
+use App\Http\Resources\MessageResource;
+use App\Models\Conversation;
+use App\Models\Group;
+use App\Models\Message;
+use App\Models\MessageAttachment;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class MessageController extends Controller
 {
-    //
+    public function byUser(User $user)
+    {
+        $messages = Message::where('sender_id', auth()->id())
+            ->where('receiver_id', $user->id)
+            ->orWhere('sender_id', $user->id)
+            ->where('receiver_id', auth()->id())
+            ->latest()
+            ->paginate(10);
+
+        return inertia('Home', [
+            'selectedConversation' => $user->toConversationArray(),
+        ]);
+    }
+
+    public function byGroup(Group $group)
+    {
+
+        $messages = Message::where('group_id', $group->id)
+            ->latest()
+            ->paginate(50);
+
+        return inertia('Home', [
+            'selectedConversation' => $group->toConversationArray(),
+            'messages' => MessageResource::collection($messages),
+        ]);
+    }
+
+    public function loadOlder(Message $message)
+    {
+        if ($message->group_id) {
+            $messages = Message::where('created_at', '<', $message->created_at)
+                ->where('group_id', $message->group_id)
+                ->latest()
+                ->paginate(10);
+        } else {
+            $messages = Message::where('created_at', '<', $message->created_at)
+                ->where(function ($query) use ($message) {
+                    $query->where('sender_id', $message->sender_id)
+                        ->where('receiver_id', $message->receiver_id)
+                        ->orWhere('sender_id', $message->receiver_id)
+                        ->where('receiver_id', $message->sender_id);
+                })
+                ->latest()
+                ->paginate(10);
+        }
+        return MessageResource::collection($messages);
+    }
+    public function store(StoreMessageRequest $request)
+    {
+        $data = $request->validated();
+        $data['sender_id'] = auth()->id();
+        $receiverId = $data['receiver_id'] ?? null;
+        $groupId = $data['group_id'] ?? null;
+
+        $files = $data['attachments'] ?? [];
+
+        $message = Message::create($data);
+
+        $attachments = [];
+
+        if ($files) {
+            foreach ($files as $file) {
+                $directory = 'attachments/' . Str::random(32);
+                Storage::makeDirectory($directory);
+
+                $model = [
+                    'message_id' => $message->id,
+                    'name' => $file->getClientOriginalName(),
+                    'mime' => $file->getClientMimeType(),
+                    'size' => $file->getSize(),
+                    'path' => $file->store($directory, 'public')
+                ];
+                $attachment = MessageAttachment::create($model);
+                $attachments[] = $attachment;
+            }
+            $message->attachments = $attachments;
+        }
+        if ($receiverId) {
+            Conversation::updateConversationWithMessage($receiverId, auth()->id(), $message);
+        }
+    }
+    public function destroy(Message $message)
+    {
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware(['auth', 'verified'])->group(function (){
     Route::get('/', [HomeController::class,'home'])->name('dashboard');
-    Route::get('/user/{user}',function () {})->name('chat.user');
-    Route::get('/group/{group}',function () {})->name('chat.group');
+    Route::get('user/{user}',[MessageController::class,'byUser'])->name('chat.user');
+    Route::get('group/{group}',[MessageController::class,'byGroup'])->name('chat.group');
+
+    Route::post('/message',[MessageController::class,'store'])->name('message.store');
+    Route::delete('/message/{message}',[MessageController::class,'destroy'])->name('message.destroy');
+    Route::get('/message/older/{message}',[MessageController::class,'loadOlder'])->name('message.loadOlder');
 });
 
 
